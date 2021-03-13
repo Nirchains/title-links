04a66a5efa8d3e01ac45d8de2209783711d0b90d
Nirchains <nirchains@gmail.com>
2021-03-13 05:02:11 +0100

    Actualización

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+import re, ast
 
-version = '0.0.1'
-requirements = parse_requirements("requirements.txt", session="")
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+
+# get version from __version__ variable in doctorus/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('title_links/__init__.py', 'rb') as f:
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 setup(
 	name='title_links',
 	version=version,
-	description='Links using DocType title instead of name as Description',
-	author='MaxMorais',
-	author_email='max.morais.dmm@gmail.com',
+	description='DoctorUS title links - Gestion del Programa de Doctorado',
+	author='HISPALIS DIGITAL',
+	author_email='pedro@hispalisdigital.com',
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
+
